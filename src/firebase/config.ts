b25322dafa5f5a3e6495876e6b1776e00a3ce0be
Sarah <saracarfix@gmail.com>
2026-02-28 20:57:42 +0000

    Console FirebaseError
    Firebase: Error (auth/configuration-not-found).

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,12 @@
-// IMPORTANT: Your Firebase project's configuration is now sourced from
-// environment variables. These are defined in your .env file.
+// IMPORTANT: Your Firebase project's configuration is defined here.
 // You can get these values from your Firebase project settings.
 // These values are safe to expose publicly.
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAXlaY1FUkxYShyA5VmkZZBhLdKFqYEHzg",
+  authDomain: "studio-6280887537-c33d8.firebaseapp.com",
+  projectId: "studio-6280887537-c33d8",
+  storageBucket: "studio-6280887537-c33d8.appspot.com",
+  messagingSenderId: "1057371755123",
+  appId: "1:1057371755123:web:7ec300b78032979bbbd006"
 };
